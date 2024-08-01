3cf521e3f459204e0b422c4ba24f71548b66162b
wangxiaoze <wangxiaoze@petalmail.com>
2024-08-01 20:38:21 +0800

    test

---
+++ src/components/TestButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <el-button class="test-btn" type="primary">Primary</el-button>
+</template>
+
+<style scoped lang="scss">
+.test-btn {
+  width: 300px;
+  height: 50px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+createApp(App).use(ElementPlus).mount('#app')
